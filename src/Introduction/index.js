83f8ce2aab1a5cdca78cb8fc2bea2e7e01b03946
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-06-26 20:02:06 +0300

    Yay! I'm learning ReactJS

---
+++ src/Introduction/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css'
+
+const Introduction  = ()=>{
+    return(
+        <div>
+            <h1 class="heading" >What I have Learnt About ReactJS</h1>
+            <p>It has reusable components.</p>
+            <p>The writing format is JSX</p>
+            <p>It is fast.</p>
+            <p>It is scalalble.</p>
+        </div>
+    );
+};
+
+export default Introduction;
