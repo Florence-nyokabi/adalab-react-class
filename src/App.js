61d2cdc17feaab5116e07a36579f37cf5fb68f7a
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-07-03 14:18:40 +0300

    Added products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Introduction from './Introduction';
+import Cards from './Cards'
 
 function App() {
   return (
     <div>
       <Introduction/>
+      <Cards/>
     </div>
   );
 }
---
+++ src/Cards/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './style.css'
+
+const Cards = ()=>{
+    return (
+        <div class="my-fruits">
+            <div class="fruit-div">
+               <img src='images/cherries.png' id="fruits"/>
+               <p>Cherries: Ksh.10.00</p>
+            </div>
+            <div class="fruit-div">
+                <img src='images/orange.png' id="fruits"/>
+                <p>Orange: Ksh 20.00</p>
+            </div>
+            <div class="fruit-div">
+                <img src="images/strawberry.png" id="fruits"/>
+                <p>Strawberry: Ksh 50.00</p>
+            </div>
+        </div>
+    );
+};
+export default Cards;
